b570c436c70eab82c646baeeed5298dab3b91bca
Huynh234 <huynhvuaa1@gmail.com>
2024-11-25 09:47:55 +0700

    huynh update hom qua

---
+++ app/src/main/java/com/example/appbooking/page/admin/homeAdmin.java
@@ -0,0 +1,77 @@
+package com.example.appbooking.page.admin;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.appbooking.R;
+import com.example.appbooking.page.admin.adRoom.quanLyPhong;
+import com.example.appbooking.page.admin.quanLyUser.quanLyUser;
+
+public class homeAdmin extends AppCompatActivity {
+    LinearLayout qlUser, qlPhong, qlDanhGia, qlMa, qlDon;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home_admin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        qlUser = findViewById(R.id.qlUser);
+        qlPhong = findViewById(R.id.qlPhong);
+        qlDanhGia = findViewById(R.id.qlDanhGia);
+        qlMa = findViewById(R.id.qlMa);
+        qlDon = findViewById(R.id.qlDon);
+
+        qlUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in1 = new Intent(homeAdmin.this, quanLyUser.class);
+                startActivity(in1);
+            }
+        });
+
+        qlPhong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in2 = new Intent(homeAdmin.this, quanLyPhong.class);
+                startActivity(in2);
+            }
+        });
+
+        qlDanhGia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        qlMa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        qlDon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/appbooking/page/admin/quanLyUser/quanLyUser.java
@@ -0,0 +1,26 @@
+package com.example.appbooking.page.admin.quanLyUser;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.appbooking.R;
+
+public class quanLyUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_quan_ly_user);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

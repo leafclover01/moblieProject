2d6fd9194d510b6e7c921000b0a1b924e5a5f966
Your Name <you@example.com>
2024-11-28 00:09:12 +0700

    fix

--- app/src/main/java/com/example/appbooking/MainActivity.java
+++ app/src/main/java/com/example/appbooking/MainActivity.java
@@ -1,109 +1,112 @@
-package com.example.appbooking;
+package com.example.appbooking.page;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsetsController;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.MenuItem;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.example.appbooking.Database.MySQLite;
-import com.example.appbooking.Model.Don;
-import com.example.appbooking.page.DashboardActivity;
-import com.example.appbooking.Model.TaiKhoan;
-import com.example.appbooking.page.admin.homeAdmin;
-import tech.turso.libsql.Database;
-import tech.turso.libsql.Libsql;
-import tech.turso.libsql.Rows;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import com.example.appbooking.R;
+import com.example.appbooking.page.customer.AccountFragment;
+import com.example.appbooking.page.customer.HistoryFragment;
+import com.example.appbooking.page.customer.HomeFragment;
+import com.example.appbooking.page.customer.OrderHotelFragment;
+import com.example.appbooking.page.customer.SettingFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
-    // khai báo tạm thời
-    EditText edtUsername, edtPassword;
-    Button btnLogIn;
-    MySQLite db;
-    ImageView ivAnh;
-    ArrayList<TaiKhoan> dstk = new ArrayList<>();
-    TextView tvError;
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_dashboard);
+
+        // Xử lý insets cho drawer layout
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        /////////////////////// Test //////////////////////////////////////////////////////////
-        db = new MySQLite();
 
-//        db.insertDataDon(1, "2024-11-23 12:00", "2024-11-26 23:00");
-//        TaiKhoan taiKhoan = new TaiKhoan();
-//        String msg = db.insertDataHoaDon(4, "2024-11-11 9:40");
-//        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+        // Thiết lập Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-//        Long msg = db.tinhSoDem("2024-11-20 14:00", "2024-11-24 10:00");
-//        String msg = db.insertDataHoaDon(5, "2024-11-11 9:40");
-//        HashMap<String, Object> map = db.tinhGiaPhong(4);
-        tvError = findViewById(R.id.tvError);
+        // Thiết lập Navigation Drawer
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-//        tvError.setText(msg.toString());
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
+        // Thay đổi fragment mặc định
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
 
-        String pathImg = db.getDrawableResourceUrl(MainActivity.this, "ic_avt");
-        ivAnh = findViewById(R.id.ivAnh);
-        ivAnh.setImageURI(Uri.parse(pathImg));
+        // Tự động thêm khoảng trống bên dưới thanh trạng thái
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.setFitsSystemWindows(true);
+    }
 
+    // Menu
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
 
-        edtUsername = findViewById(R.id.edtUsername);
-        edtPassword = findViewById(R.id.edtPassword);
+        if (id == R.id.nav_home) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        } else if (id == R.id.nav_settings) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new SettingFragment())
+                    .commit();
+        } else if (id == R.id.nav_hotel) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new OrderHotelFragment())
+                    .commit();
+        } else if (id == R.id.nav_account) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new AccountFragment())
+                    .commit();
+        } else if (id == R.id.nav_history_survey) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new HistoryFragment())
+                    .commit();
+        }
 
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
-        btnLogIn = findViewById(R.id.btnLogIn);
-        btnLogIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String tenDangNhap = edtUsername.getText().toString().trim();
-                String matKhau = edtPassword.getText().toString().trim();
-                if (tenDangNhap.length() > 0 && matKhau.length() > 0) {
-                    TaiKhoan taiKhoan = db.kiemTraDangNhap(tenDangNhap, matKhau);
-                    String msg = taiKhoan.getRole() == 0 ? "dung" : "sai";
-//                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    if (taiKhoan.getId() >= 0 && taiKhoan.getRole() >= 0) {
-                        if (taiKhoan.getRole() == 0) {
-                            Intent intentQuanTri = new Intent(MainActivity.this, homeAdmin.class);
-                            intentQuanTri.putExtra("taiKhoan", taiKhoan);
-                            startActivity(intentQuanTri);
-                        }
-                        if (taiKhoan.getRole() == 1) {
-                            Intent intentDatHang = new Intent(MainActivity.this, DashboardActivity.class);
-                            intentDatHang.putExtra("taiKhoan", taiKhoan);
-                            startActivity(intentDatHang);
-                        }
-                    } else Toast.makeText(MainActivity.this, "Tài khoản hoặc mật khẩu không đúng!", Toast.LENGTH_SHORT).show();
-                } else Toast.makeText(MainActivity.this, "Vui lòng nhập tài khoản và mật khẩu!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-            /////////////////////// Test /////////////////////////////////////////////////////////
-
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
